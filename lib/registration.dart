35932aa504c6bfe5b6ffcfa9b183a7b145de4376
kirancherote9745 <mailboxkirancherote@gmail.com>
2024-03-16 12:16:27 +0530

    commit
    commit

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:js_interop';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
 
 class Regist extends StatefulWidget {
@@ -6,16 +10,46 @@ class Regist extends StatefulWidget {
   @override
   State<Regist> createState() => _RegistState();
 }
-
+// text editing controller
 class _RegistState extends State<Regist> {
+  final namecontroller = TextEditingController();
+  final dateofbirthcontroller = TextEditingController();
+  final emailcontroller = TextEditingController();
+  final password = TextEditingController();
+
+  bool loading = false;
+
+
+  void register() async{
+    print(namecontroller.text);
+    print(dateofbirthcontroller.text);
+    print(emailcontroller.text);
+    print(password.text);
+    
+    Uri uri = Uri.parse('https://scnner-web.onrender.com/api/register');
+
+    var response = await http.post(uri,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: jsonEncode(
+    <String, String>{
+    'name':namecontroller.text,
+    'dob':dateofbirthcontroller.text,
+    'email':emailcontroller.text,
+    'password':password.text,
+    }),
+    );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       home: Scaffold(
+        backgroundColor: Colors.indigo,
           appBar: AppBar(
             title: Text('Registration Page'),
-            backgroundColor: Colors.indigo,
+            backgroundColor: Colors.indigoAccent,
           ),
           body: Column(
             children: [
@@ -23,6 +57,8 @@ class _RegistState extends State<Regist> {
                 height: 50,
               ),
               TextField(
+
+                controller: namecontroller,
                 decoration: InputDecoration(
                     hintText: 'Enter your Name',
                     border: OutlineInputBorder(
@@ -33,6 +69,7 @@ class _RegistState extends State<Regist> {
                 height: 10,
               ),
               TextField(
+                controller: dateofbirthcontroller,
                 decoration: InputDecoration(
                     hintText: 'Enter Date of Birth',
                     border: OutlineInputBorder(
@@ -43,6 +80,7 @@ class _RegistState extends State<Regist> {
                 height: 10,
               ),
               TextField(
+                controller: emailcontroller,
                 decoration: InputDecoration(
                     hintText: 'Enter your Email',
                     border: OutlineInputBorder(
@@ -53,6 +91,7 @@ class _RegistState extends State<Regist> {
                 height: 10,
               ),
               TextField(
+                controller: password ,
                 decoration: InputDecoration(
                     hintText: 'Enter your Password',
                     border: OutlineInputBorder(
@@ -63,7 +102,9 @@ class _RegistState extends State<Regist> {
                 height: 20,
               ),
               GestureDetector(
-                onTap: () {},
+                onTap: () {
+                 register();
+                },
                 child: Container(
                   height: 50,
                   width: 100,
